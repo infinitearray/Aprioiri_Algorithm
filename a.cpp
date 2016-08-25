c72f0802a0d2d058ea8111c1ca80906ae50da07e
infinitearray <bandiharshav97@gmail.com>
2016-08-26 01:06:34 +0530

    Read from config file

--- a.cpp
+++ a.cpp
@@ -4,14 +4,27 @@ using namespace std;
 
 int main()
 {
-  string line,input,output;
+  string temp,line,input,output;
+  int i;
+  float confidence,support,flag;
   ifstream myfile("config.csv");
   if(myfile.is_open())
   {
+    i=0;
     while(getline(myfile, line))
     {
-      cout << line.substr(line.find(",")+1,line.length()-1) << "\n";
-
+      temp = line.substr(line.find(",")+1,line.length()-1);
+      if(i==0)
+        input=temp;
+      else if(i==1)
+        output=temp;
+      else if(i==2)
+        support=std::atof(temp.c_str());
+      else if(i==3)
+        confidence=std::atof(temp.c_str());
+      else if(i==4)
+        flag=std::atof(temp.c_str());
+      i++;
     }
     myfile.close();
   }
