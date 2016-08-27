2bd088d5d1bb37d759eae9857ce358dc26bb3f26
infinitearray <bandiharshav97@gmail.com>
2016-08-27 17:09:28 +0530

    Read from input file

--- a.cpp
+++ a.cpp
@@ -1,11 +1,31 @@
 #include<bits/stdc++.h>
+#include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
+
+void splitter(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+vector<string> split(const string &s, char delim) {
+    vector<string> elems;
+    splitter(s, delim, elems);
+    return elems;
+}
+
+vector< vector<string> > Input;
 
 int main()
 {
   string temp,line,input,output;
   int i;
+  vector<string> lines;
   float confidence,support,flag;
   ifstream myfile("config.csv");
   if(myfile.is_open())
@@ -27,8 +47,29 @@ int main()
       i++;
     }
     myfile.close();
+    ifstream inputfile("input.csv");
+    if(inputfile.is_open())
+    {
+      while(getline(inputfile, line))
+      {
+        lines.erase(lines.begin(), lines.end());
+        lines = split(line, ',');
+        Input.push_back(lines);
+        lines.clear();
+      }
+    }
+    else
+      cout << "Unable to open input file\n";
+    for(int i=0;i<Input.size();i++)
+    {
+      for(int j=0;j<Input[i].size();j++)
+      {
+        cout << Input[i][j] << " ";
+      }
+      cout << "\n";
+    }
   }
   else
-    cout << "Unable to open file\n";
+    cout << "Unable to open config file\n";
   return 0;
 }
